ffc2340320d47e43d0634dc884fa17ae686e7eae
Mike Castle <dalgoda@gmail.com>
2023-11-25 19:38:26 -0800

    Use replacement API for logging memory usage.

--- py/app.py
+++ py/app.py
@@ -76,7 +76,7 @@ def run(func):
     os.symlink(long_pathname, short_pathname)
     ret = func(parser)
     logging.info('Max memory used: %s',
-                 humanize.binary_size(
+                 humanize.naturalsize(
                      resource.getrusage(resource.RUSAGE_SELF).ru_maxrss))
     logging.info('Finished. (%d)', ret or 0)
     return ret
